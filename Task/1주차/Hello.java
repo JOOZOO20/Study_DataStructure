8463f4e3790e6a1d3b259f3204936b6863582daf
JOOZOO20 <62732461+JOOZOO20@users.noreply.github.com>
2022-06-05 01:25:16 +0900

    Update Hello.java

--- Task/1주차/Hello.java
+++ Task/1주차/Hello.java
@@ -3,9 +3,9 @@ package lab0_1;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("lab0_1 : Â÷ÈñÁÖ");
+		System.out.println("lab0_1 : Ã‚Ã·ÃˆÃ±ÃÃ–");
 		System.out.println("Hello World!");
 
 	}
 
-}
+} 
